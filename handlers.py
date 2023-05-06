2a03b4d671eae76f58099d7860fa7f916124a722
avinash_yadav11 <avinash.yadav@juspay.in>
2023-05-06 11:17:20 +0530

    Backend Repo Created

---
+++ handlers.py
@@ -0,0 +1,387 @@
+import psycopg2
+from flask import jsonify
+
+def getConnectionDetails():
+  conn = psycopg2.connect(
+    host="35.244.45.154",
+    database="final-project",
+    user="postgres",
+    password="avi2014#"
+    )
+  return conn
+
+# conn = getConnectionDetails()
+
+def registrationHandler(name,email,password,role,dob):
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "INSERT INTO user_details (name, email, password, role, verified, dob) VALUES (%s, %s, %s, %s, %s, %s)"
+  values = (name,email,password,role,False,dob)
+  try:
+    cur.execute(query, values)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return True
+  except Exception as e:
+    cur.close()
+    conn.close()
+    print(e)
+    return False
+  
+def loginHandler(email,password):
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "Select * from user_details where email = %s"
+  values = (email,)
+  try:
+    r = cur.execute(query,values)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    # print(rows)
+    if rows[0][-4] == password:
+        return rows
+    else:
+        return False
+  except Exception as e:
+    print(e)
+    cur.close()
+    conn.close()
+    return False
+
+def checkRole(userId, role):
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "Select * from user_details where id = %s"
+  values = (userId,)
+  try:
+    r = cur.execute(query,values)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    # print(rows)
+    if rows[0][-3] == role:
+      return True
+    else:
+      return False
+  except Exception as e:
+    print(e)
+    cur.close()
+    conn.close()
+    return False
+  
+def getDueApprovals(userId):
+  d = checkRole(userId,"admin")
+  if d == False:
+    return "User must be ADMIN"
+  else:
+    return dueApprovals()
+
+def dueApprovals():
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "Select * from user_details where verified = %s"
+  values = (False,)
+  try:
+    r = cur.execute(query,values)
+    rows = cur.fetchall()
+    # print(rows)
+    cur.close()
+    conn.close()
+    return rows
+  except:
+    cur.close()
+    conn.close()
+    return False
+
+def approvalHandler(userIds, adminId):
+  d = checkRole(adminId,"admin")
+  if d == False:
+    return "USER MUST BE ADMIN"
+  for userId in userIds:
+    g = approveUser(userId)
+    h = assignTable(userId)
+    if g == False :
+      return False
+  return True
+  
+def assignTable(userId):
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "Select * from user_details where id = %s"
+  values = (userId,)
+  r = cur.execute(query,values)
+  rows = cur.fetchall()
+  row = rows[0]
+  role = row[-3]
+  if role == "faculty":
+    query = "Insert INTO faculties (user_id) values (%s)"
+    r = cur.execute(query,values)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return True
+  elif role == "student":
+    query = "INSERT INTO students (user_id) values (%s)"
+    r = cur.execute(query,values)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return True
+  return True
+
+def approveUser(userId):
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "UPDATE user_details SET verified = %s WHERE id = %s"
+  # print(userId)
+  values = (True,userId)
+  try: 
+    r = cur.execute(query,values)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return True
+  except Exception as e:
+    print(e)
+    cur.close()
+    conn.close()
+    return False
+
+def editProfile(userId,oldPassword,newPassword):
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "select * from user_details where id = %s"
+  values = (userId,)
+  try:
+    r = cur.execute(query,values)
+    rows = cur.fetchall()
+    # cur.close()
+    # conn.close()
+    if rows[0][-4] == oldPassword:
+      queryNew = "Update user_details SET password = %s where id = %s"
+      valuesnew = (newPassword,userId)
+      r = cur.execute(queryNew,valuesnew)
+      conn.commit()
+      cur.close()
+      conn.close()
+      return True
+  except Exception as e:
+    print(e)
+    cur.close()
+    conn.close()
+    return False
+  
+def assignAssignment(userId, questions, batchId, courseId):
+  
+  questions = "##;;".join(questions)
+  d = checkRole(userId,"faculty")
+  if d == False:
+    return "User must be Faculty"
+  else:
+    try:
+      conn = getConnectionDetails()
+      cur = conn.cursor()
+      query = "Insert into assignment (description,faculty_id,batch_id,course_id) values (%s,%s,%s,%s)"
+      values = (questions,userId,batchId,courseId)
+      r = cur.execute(query,values)
+      conn.commit()
+      cur.close()
+      conn.close()
+      return True
+    except Exception as e:
+      return False
+
+def getFacultyBatchCourseDetails(userId):
+  d = checkRole(userId,"faculty")
+  if d == False:
+    return "User must be faculty"
+  else:
+    try:
+      conn = getConnectionDetails()
+      cur = conn.cursor()
+      query = "Select * from faculties where user_id = %s"
+      values = (userId,)
+      r = cur.execute(query,values)
+      rows = cur.fetchall()
+      print(rows)
+      row = rows[0]
+      batches = row[-2]
+      courses = row[-1]
+      print(batches,courses)
+      batch = resolveBatches(batches)
+      course = resolveCourses(courses)
+      return (batch,course)
+    except Exception as e:
+      print(e)
+      return False
+
+def resolveBatches(batches):
+  batchDetails = []
+  for batch in batches:
+    conn = getConnectionDetails()
+    cur = conn.cursor()
+    query = "Select * from batch where id = %s"
+    values = (batch,)
+    r = cur.execute(query,values)
+    rows = cur.fetchall()
+    row = rows[0]
+    j = {}
+    j['batch_id'] = row[0]
+    j['batch_code'] = row[1]
+    j['batch_name'] = row[2]
+    batchDetails.append(j)
+    cur.close()
+    conn.close()
+  return ({"batches":batchDetails})
+
+def resolveCourses(courses):
+  courseDetails = []
+  for course in courses:
+    j = getSingleCourseDetails(course)
+    courseDetails.append(j)
+  return ({"courses":courseDetails})
+
+def getSingleCourseDetails(course):
+  conn = getConnectionDetails()
+  cur = conn.cursor()
+  query = "Select * from course where id = %s"
+  values = (course,)
+  r = cur.execute(query,values)
+  rows = cur.fetchall()
+  row = rows[0]
+  j = {}
+  j['course_id'] = row[0]
+  j['course_code'] = row[1]
+  j['course_name'] = row[2]
+
+  cur.close()
+  conn.close()
+  return j
+
+def viewAssignment(userId):
+  d = checkRole(userId,"student")
+  if d == False:
+    return "User must be student"
+  else:
+    try:
+      conn = getConnectionDetails()
+      cur = conn.cursor()
+      query = "Select * from students where user_id = %s"
+      values = (userId,)
+      r = cur.execute(query,values)
+      rows = cur.fetchall()
+      row = rows[0]
+      batchId = row[-1]
+      print(batchId)
+      queryNew = "Select * from assignment where batch_id = %s"
+      valueNew = (batchId,)
+      r = cur.execute(queryNew,valueNew)
+      rows = cur.fetchall()
+      fOutput = []
+      for row in rows:
+        course = row[-1]
+        description = row[1]
+        questions = description.split("##;;")
+        j = {}
+        j["questions"] = questions
+        j["course"] = getSingleCourseDetails(course)
+        fOutput.append(j)
+      return fOutput
+    except Exception as e:
+      return False
+
+# def viewTimetable(userId):
+def getAllCourses(userId):
+  d = checkRole(userId,"admin")
+  if d == False:
+    return "User Must be Admin"
+  try:
+    conn = getConnectionDetails()
+    cur = conn.cursor()
+    query = "SELECT * from courses"
+    rows = query.execute()
+    result = []
+    for row in rows:
+      j = {}
+      j['course_id'] = row[0]
+      j['course_code'] = row[1]
+      j['course_name'] = row[2]
+      result.append(j)
+    cur.close()
+    conn.close()
+    return result
+  except:
+    cur.close()
+    conn.close()
+    return False
+  
+# def assignCourse(userId, courseCode, facultyId): Define this function
+
+def addNewCourse(userId,courseCode,courseName):
+  d = checkRole(userId, "admin")
+  if d == False:
+    return "User must be admin"
+  try:
+    conn = getConnectionDetails()
+    cur = conn.cursor()
+    query = "INSERT INTO course (code, name) VALUES (%s, %s)"
+    values = (courseCode,courseName)
+    r = cur.execute(query, values)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return True
+  except:
+    return False    
+
+def uploadNotice(userId, batchId, courseId, noticeData):
+  d = checkRole(userId,"faculty")
+  if d == False:
+    return "User must be a staff"
+  try:
+    conn = getConnectionDetails()
+    cur = conn.cursor()
+    query = "INSERT INTO notice (description, course_id, batch_id, faculty_id) values (%s, %s, %s, %s)"
+    values = (noticeData, courseId, batchId, userId)
+    r = cur.execute(query,values)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return True
+  except:
+    cur.close()
+    conn.close()
+    return False
+  
+def uploadFeedback(userId, courseId, feedback):
+  d = checkRole(userId,"student")
+  if d == False:
+    return "User must be a student"
+  try:
+    query = "INSERT INTO feedback (content, student_id , course_id) VALUES (%s, %s, %s)"
+    values = (feedback, userId, courseId)
+    conn = getConnectionDetails()
+    cur = conn.cursor()
+    r = cur.execute(query,values)
+    conn.commit()
+    conn.close()
+    cur.close()
+    return True
+  except:
+    return False
+  
+# def getStudentBatchCourseDetails(userId):
+#   d = checkRole(userId,"student")
+#   if d == False:
+#     return "User must be a student"
+#   try:
+#     conn = getConnectionDetails()
+#     cur = conn.cursor()
+#     query = "Select * from students where user_ud = %s"
+#     values = (userId)
+
+
+# print(approvalHandler(["3"],"2"))
+# print(loginHandler("mt09@gmail.com","manish123"))
---
+++ route.py
@@ -0,0 +1,195 @@
+from flask import Flask,request,jsonify
+from flask_restful import Resource, Api, reqparse
+import handlers as handler
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(app)
+
+
+@app.route("/",methods=['GET'])
+def healthCheck():
+    return jsonify({"result":"Hello World"})
+
+@app.route('/registration',methods = ['POST'])
+def registration(): #Done
+    if request.method == 'POST':
+        print("Incoming Request")
+        name = request.json['name']
+        email = request.json['email']
+        password = request.json['password']
+        role = request.json['role']
+        dob = request.json['dob']
+        return jsonify({"result":handler.registrationHandler(name,email,password,role,dob)})
+
+@app.route('/login',methods = ['POST'])
+def login(): #Done
+    email = request.json['email']     
+    password = request.json['password']
+    return jsonify({"result":handler.loginHandler(email,password)})
+
+@app.route('/approval',methods = ['POST','GET'])
+def approvalAdmin(): #Done
+    if request.method == 'POST':
+        userId = request.json['user_id']
+        adminId = request.json ['admin_id']
+        return jsonify({"result":handler.approvalHandler(userId,adminId)})
+    if request.method == 'GET':
+        adminId = request.args.get('user_id')
+        return jsonify({"result":handler.getDueApprovals(adminId)}) 
+@app.route('/editProfile', methods = ['POST'])
+def editProfile(): #Done
+    userId = request.json['user_id']
+    passwordOld = request.json['old_password']
+    passwordNew = request.json['new_password']
+    return jsonify({"result":handler.editProfile(userId,passwordOld,passwordNew)})
+
+@app.route('/assignment', methods =['GET','POST'])
+def assignAssignment(): #Done
+    if request.method == 'POST':
+        userId = request.json['user_id']
+        questions = request.json['questions']
+        batchId = request.json['batch']
+        courseId = request.json['course']
+        return jsonify({"result":handler.assignAssignment(userId, questions, batchId, courseId)})
+    if request.method == 'GET':
+        userId = request.args.get('user_id')
+        # print(userId)
+        return jsonify({"result":handler.getFacultyBatchCourseDetails(userId)})
+
+@app.route('/viewAssignment', methods=['GET'])
+def viewAssignment(): #Done
+    userId = request.args.get('user_id')
+    return jsonify({"result":handler.viewAssignment(userId)})
+
+# @app.route('/viewTimetable', methods=['GET'])
+# def viewTimetable(): #Pending
+#     userId = request.json['user_id']
+#     return jsonify({"result":handler.viewTimetable(userId)})
+
+# @app.route('/uploadTimetable', methods=['POST'])
+# def uploadTimetable(): #Pending
+#     userId = request.json['user_id']
+#     data = request.json['data']
+#     return jsonify({"result":handler.viewTimetable(userId,data)
+# #Define GET METHO})D
+
+# @app.route('/feedback', methods =['POST','GET'])
+# def uploadFeedback(): 
+#     if request.method == 'POST':
+#         userId = request.json['user_id']
+#         courseId = request.json['course_id']
+#         feedback = request.json ['feedback']
+#         return jsonify({"result":handler.uploadFeedback(userId,courseId,feedback)})
+
+#     if request.method == 'GET':
+#         userId = request.jargs.get('user_id')         return jsonify({"result":handler.getStudentBatchCourseDetails(userId)}) 
+# @app.route('/uploadInternalResult', methods = ['POST','GET'])
+# def uploadInternalResult(): #Pending
+#     if request.method == 'POST':
+#         userId = request.json['user_id']
+#         data = request.json['data']
+#         batchId = request.json['batch_id']
+#         courseId = request.json['course_id']
+#         return jsonify({"result":handler.uploadInternalResult(userId,data,batchId,courseId)}) 
+#     if request.method == 'GET':
+#         userId = request.jargs.get('user_id')         return jsonify({"result":handler.getFacultyBatchCourseDetails(userId)})
+
+# @app.route('/uploadExternalResult', methods = ['POST', 'GET'])
+# def uploadExternalResult(): #Pending
+#     if request.method == 'POST':
+#         userId = request.json['user_id']
+#         data = request.json['data']
+#         batchId = request.json['batch_id']
+#         courseId = request.json['course_id']
+#         return jsonify({"result":handler.uploadExternalResult(userId,data,batchId,courseId)
+#     if request.method == 'GET'}):
+#         return jsonify({"result":handler.getAllBatchCourseDetails()}) 
+@app.route('/notice', methods = ['POST','GET'])
+def uploadNotice(): #Done
+    if request.method == 'POST':
+        userId = request.json['user_id']
+        batchId = request.json['batch_id']
+        courseId = request.json['course_id']
+        noticeData = request.json['notice_data']
+        return jsonify({"result":handler.uploadNotice(userId,batchId,courseId,noticeData)})
+    if request.method == 'GET':
+        userId = request.jargs.get('user_id')       
+        return jsonify({"result":handler.getFacultyBatchCourseDetails(userId)})
+# @app.route('/viewNotice',methods = ['POST','GET'])
+# def viewNotice(): #Pending
+#     if request.methods == 'POST':
+#         batchId = request.json['batch_id']
+#         courseId = request.json['course_id']
+#         return jsonify({"result":handler.viewNotice(batchId,courseId)
+#     if request.methods == 'GET'}):
+#         return jsonify({"result":handler.getAllBatchCourseDetails()}) 
+# @app.route('/uploadAttendance',methods=['POST','GET'])
+# def attendanceDetails(): #Pending
+#     if request.methods == 'POST':
+#         userId = request.json['user_id']
+#         batchId = request.json['batch_id']
+#         courseId = request.json['course_id']
+#         date = request.json['date']
+#         students_list = request.json['students_list']
+#         attendace_list = request.json['attendance_list']
+#         return jsonify({"result":handler.uploadAttendance(userId,batchId,courseId,date,students_list,attendace_list)})
+
+#     if request.methods == 'GET':
+#         userId = request.json['user_id']
+#         return jsonify({"result":handler.getFacultyBatchCourseDetails()}) 
+# @app.route('/viewResult',methods = ['POST','GET'])
+# def resultDetails(): #Pending
+#     if request.methods == 'POST':
+#         userId = request.json['user_id']
+#         return jsonify({"result":handler.getResult(userId)}) 
+
+# @app.route('/viewAttendance', methods = ['POST','GET'])
+# def viewAttendance(): #Pending
+#     if request.methods == 'POST':
+#         userId = request.json['user_id']
+#         courseId = request.json['course_id']
+#         return jsonify({"result":handler.viewAttendance(userId,courseId)
+#     if request.methods == 'GET'}):
+#         userId = request.json['user_id']
+#         return jsonify({"result":handler.getStudentBatchCourseDetails(userId)}) 
+# @app.route('/editStudentDetails', methods = ['POST','GET','DELETE'])
+# def updateStudentDetails(): #Pending
+#     if request.methods == 'POST':
+#         userId = request.json['user_id']
+#         studentId = request.json['student_id']
+#         email = request.json['new_email']
+#         dob = request.json['dob']
+#         name = request.json['name']
+#         return jsonify({"result":handler.updateStudentDetails(userId,studentId,email,dob,name) #This POST request will Update the student details
+#     if request.methods == 'GET'}):
+#         userId = request.json['user_id']
+#         return jsonify({"result":handler.getFacultyStudents(userId) #A Faculty can change Student Details. This GET request will fetch all the students under the faculty
+#     if request.methods == 'DELET})E': 
+#         userId = request.json['user_id']
+#         studentId = request.json['student_id']
+#         return jsonify({"result":handler.deleteStudent(userId,studentId) #This Delete request will delete the user})
+
+@app.route('/addCourse')
+def addCourse(): #Done
+    if request.methods == 'POST':
+        userId = request.json['user_id']
+        courseName = request.json['course_name']
+        courseCode = request.json['course_code']
+        return jsonify({"result":handler.addNewCourse(userId,courseCode,courseName)}) 
+    
+# @app.route('/assignCourse', methods = ['POST','GET'])
+# def assignCourse(): #Pending
+#     if request.methods == 'POST':
+#         userId = request.json['user_id']
+#         courseCode = request.json['course_code']
+#         facultyId = request.json['faculty_id']
+#         return jsonify({"result":handler.assignCourse(userId,courseCode,facultyId)
+#     if request.methods == 'GET'}):
+#         userId = request.json['user_id']
+#         return jsonify({"result":handler.getAllCourses(userId)})
+
+if __name__ == '__main__':
+    app.run()
